97ddd679583b365092c2198719a53c422517ec87
Jeronimo Fagundes <jeronimo.fagundes@king.host>
2019-03-08 15:17:44 -0300

    First version of classes to run dig

--- src/Dig.php
+++ src/Dig.php
@@ -1,7 +1,114 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: jeronimo
- * Date: 08/03/19
- * Time: 12:14
+ * Runs the unix command dig.
+ * @author Jeronimo Fagundes <jeronimo.fs@protonmail.com>
  */
+
+namespace JeronimoFagundes\PhpDig;
+
+use \InvalidArgumentException;
+use \RuntimeException;
+
+class Dig {
+    protected $config = null;
+    protected $name = null;
+    protected $type = 'A';
+
+    /**
+     * Dig constructor.
+     * @param string $name The name to query.
+     * @param string $type The DNS record type to query. Default is A.
+     * @param DigConfig $config The config for the command dig.
+     */
+    public function __construct($name = null, $type = 'A', $config = new DigConfig){
+        $type = trim($type);
+        if (!$this->typeIsSupported($type)) {
+            throw new InvalidArgumentException(
+                "type {$type} not supported. Supported types are: " .
+                implode(',', $this->supportedTypes())
+            );
+        }
+
+        if (is_string($name)) { $name = trim($name); }
+        if (empty($name) || !is_string($name)) { //TODO: Really check if it is a hostname
+            throw new InvalidArgumentException(
+                "name needs to be a hostname"
+            );
+        }
+
+        $this->type = $type;
+        $this->name = $name;
+        $this->config = $config;
+    }
+
+    /**
+     * Gets a list of all supported types
+     * @return array
+     */
+    protected function supportedTypes(){
+        // The following list of supported types was obtained in https://en.wikipedia.org/wiki/List_of_DNS_record_types
+        // The obsolete types are not supported.
+        return array(
+            'A', 'AAAA', 'AFSDB', 'APL', 'AXFR',
+            'CAA', 'CDNSKEY', 'CDS', 'CERT', 'CNAME',
+            'DHCID', 'DLV', 'DNAME', 'DNSKEY', 'DS',
+            'HIP',
+            'IPSECKEY', 'IXFR',
+            'KEY', 'KX',
+            'LOC',
+            'MX',
+            'NAPTR', 'NS', 'NSEC', 'NSEC3', 'NSEC3PARAM',
+            'OPENPGPKEY', 'OPT',
+            'PTR',
+            'RRSIG', 'RP',
+            'SIG', 'SOA', 'SRV', 'SSHFP',
+            'TA', 'TKEY', 'TLSA', 'TSIG', 'TXT',
+            'URI'
+        );
+    }
+
+    /**
+     * Tells if a type is supported or not.
+     * @param $type string The type to test.
+     * @return bool
+     */
+    protected function typeIsSupported($type){
+        return is_string($type) && in_array($type, $this->supportedTypes());
+    }
+
+    /**
+     * Builds the command line to run.
+     * @return string
+     */
+    protected function buildCommandLine(){
+        return
+            "dig " .
+            ($this->config->getServer()) .
+            " " .
+            ($this->config->getTimeout()) .
+            " " .
+            ($this->config->getTries()) .
+            " " .
+            $this->config->getShort() .
+            " " .
+            " {$this->name} {$this->type}";
+    }
+
+    /**
+     * Query the server for the pair name + type
+     * @return array The return of dig execution
+     */
+    public function query(){
+        $commandLine = $this->buildCommandLine();
+        $return = array();
+        $intReturn = 0;
+        exec($commandLine, $return, $intReturn);
+
+        if ($intReturn != 0){
+            throw new \http\Exception\RuntimeException('dig execution returned a code different from 0');
+        }
+
+        return $return;
+    }
+
+}
--- src/DigConfig.php
+++ src/DigConfig.php
@@ -1,15 +1,97 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: jeronimo
- * Date: 08/03/19
- * Time: 12:22
+ * Configuration for the dig command.
+ * @author Jeronimo Fagundes <jeronimo.fs@protonmail.com>
  */
 
 namespace JeronimoFagundes\PhpDig;
+use \InvalidArgumentException;
 
 
 class DigConfig
 {
+    protected $tries = 3;
+    protected $timeout = 5;
+    protected $server = null;
+    protected $short = true; // Allowing only short for now.
 
+    public function __construct(){
+    }
+
+    /**
+     * Sets the number of times to try UDP queries to server to $tries instead of the default, 3.
+     * If $tries is less than or equal to zero, the number of tries is silently rounded up to 1.
+     * @param int $tries The number of times to try UDP queries to server
+     * @throws \InvalidArgumentException If $tries is not an integer
+     * @return void
+     */
+    public function setTries($tries = 3){
+        if(!is_int($tries)) {
+            throw new \http\Exception\InvalidArgumentException('tries needs to be an integer')
+        }
+
+        $this->tries = $tries;
+    }
+
+    /**
+     * Sets the timeout for a query to $timeout seconds. The default timeout is 5 seconds.
+     * An attempt to set $timeout to less than 1 will result in a query timeout of 1 second being applied.
+     * @param int $timeout The timeout in seconds for a query
+     * @throws \InvalidArgumentException If $timeout is not an integer
+     * @return void
+     */
+    public function setTimeout($timeout = 5){
+        if(!is_int($timeout)) {
+            throw new \http\Exception\InvalidArgumentException('timeout needs to be an integer')
+        }
+
+        $this->timeout = $timeout;
+    }
+
+    /**
+     * Sets the name or IP address of the name server to query.
+     * @param string $server The name or IP address of the name server to query
+     * @throws \InvalidArgumentException If $server is not a string
+     * @return void
+     * @todo Validate if the string passed in $server is a hostname or IP.
+     */
+    public function setServer($server = null){
+        if(!is_string($server)) {
+            throw new \http\Exception\InvalidArgumentException('server needs to be an ip or hostname')
+        }
+
+        $this->server = $server;
+    }
+
+    /**
+     * Gets the string to be passed to dig to configure tries.
+     * @return string
+     */
+    public function getTries(){
+        return "+tries={$this->tries}";
+    }
+
+    /**
+     * Gets the string to be passed to dig to configure timeout.
+     * @return string
+     */
+    public function getTimeout(){
+        return "+timeout={$this->timeout}";
+    }
+
+    /**
+     * Gets the string to be passed to dig to configure server.
+     * @return string
+     */
+    public function getServer(){
+        return empty($this->server) ? "" : "@{$this->server}";
+    }
+
+    /**
+     * Gets the string to be passed to dig to configure the shortness.
+     * @return string
+     */
+    public function getShort(){
+        return $this->short ? "+short" : "";
+    }
 }
