5cdf44cbb9f53a82f621fb66baf9f7846efd8b09
Jeronimo Fagundes <jeronimo.fagundes@king.host>
2019-03-08 15:17:19 -0300

    Added .idea (from phpstorm) to .gitignore

---
+++ src/Dig.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeronimo
+ * Date: 08/03/19
+ * Time: 12:14
+ */
---
+++ src/DigConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeronimo
+ * Date: 08/03/19
+ * Time: 12:22
+ */
+
+namespace JeronimoFagundes\PhpDig;
+
+
+class DigConfig
+{
+
+}
